41d62dfe49e3e3f237543bb187a9d33f335b2025
Fausio Matsinhe <fausio.matsinhe@kooli.io>
2023-04-17 15:58:59 +0200

    HouseholdController

---
+++ Controllers/HouseholdController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Web.Resource;
+
+namespace csi.asp.net.core.api.Controllers
+{
+
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    [RequiredScope(RequiredScopesConfigurationKey = "AzureAd:Scopes")]
+    public class HouseholdController
+    {
+    }
+}

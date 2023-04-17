78fcc5b17892362d98af357d46821a4e38ff02da
Fausio Matsinhe <fausio.matsinhe@kooli.io>
2023-04-17 16:31:10 +0200

    DbSet

--- csi.asp.net.core.data/App.Context/CSI_AppContext.cs
+++ csi.asp.net.core.data/App.Context/CSI_AppContext.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using csi.asp.net.core.model.model;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,13 @@ using System.Threading.Tasks;
 
 namespace csi.asp.net.core.data.App.Context
 {
-    public class CSI_AppContext
+    public class CSI_AppContext  : DbContext  
     {
+
+        public DbSet<Site> sites { get; set; }
+        public DbSet<CollaboratorRole>  collaboratorRoles { get; set; }
+        public DbSet<Partner>  partners { get; set; }
+        public DbSet<HouseHold>  houseHolds { get; set; }
+       
     }
 }

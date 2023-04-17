db524347cd81c560e3b44e8fbba9961f89df6e65
Fausio Matsinhe <fausio.matsinhe@kooli.io>
2023-04-17 15:59:12 +0200

    SiteController

---
+++ Controllers/SiteController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Web.Resource;
+
+namespace csi.asp.net.core.api.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    [RequiredScope(RequiredScopesConfigurationKey = "AzureAd:Scopes")] 
+    public class SiteController
+    {
+    }
+}

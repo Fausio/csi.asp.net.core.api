bd072bb537457d003b3281ed5e2aed202ea01969
Fausio Matsinhe <fausio.matsinhe@kooli.io>
2023-04-18 08:54:44 +0200

    seed

--- csi.asp.net.core.data/Seed/SeedData.cs
+++ csi.asp.net.core.data/Seed/SeedData.cs
@@ -12,44 +12,30 @@ namespace csi.asp.net.core.data.Seed
     {
 
 
-        private static List<Site> SiteToSeed = new List<Site> { new Site() { CreatedDate = new DateTime(), Name = "OCB Principal" } };
+        private static List<Site> SiteToSeed = new List<Site> { new Site() { Guid = Guid.NewGuid(), CreatedDate = DateTime.Now, Name = "OCB Principal" } };
 
 
         private static List<Partner> PartnerToSeed()
         {
             List<Partner> data = new List<Partner>();
 
-            data.Add(new Partner() { Guid = Guid.NewGuid(), Name = "partner 1", CreatedDate = new DateTime(), Site = SiteToSeed.FirstOrDefault(), CollaboratorRoleId = 1 });
-            data.Add(new Partner() { Guid = Guid.NewGuid(), Name = "partner 2", CreatedDate = new DateTime(), Site = SiteToSeed.FirstOrDefault(), CollaboratorRoleId = 1 });
+            data.Add(new Partner() { Guid = Guid.NewGuid(), Name = "partner 1", CreatedDate = DateTime.Now, Site = SiteToSeed.FirstOrDefault(), CollaboratorRoleId = 1 });
+            data.Add(new Partner() { Guid = Guid.NewGuid(), Name = "partner 2", CreatedDate = DateTime.Now, Site = SiteToSeed.FirstOrDefault(), CollaboratorRoleId = 1 });
 
-            data.Add(new Partner() { Guid = Guid.NewGuid(), Name = "partner 3", CreatedDate = new DateTime(), Site = SiteToSeed.FirstOrDefault(), CollaboratorRoleId = 2 });
-            data.Add(new Partner() { Guid = Guid.NewGuid(), Name = "partner 4", CreatedDate = new DateTime(), Site = SiteToSeed.FirstOrDefault(), CollaboratorRoleId = 2 });
-            data.Add(new Partner() { Guid = Guid.NewGuid(), Name = "partner 5", CreatedDate = new DateTime(), Site = SiteToSeed.FirstOrDefault(), CollaboratorRoleId = 2 });
+            data.Add(new Partner() { Guid = Guid.NewGuid(), Name = "partner 3", CreatedDate = DateTime.Now, Site = SiteToSeed.FirstOrDefault(), CollaboratorRoleId = 2 });
+            data.Add(new Partner() { Guid = Guid.NewGuid(), Name = "partner 4", CreatedDate = DateTime.Now, Site = SiteToSeed.FirstOrDefault(), CollaboratorRoleId = 2 });
+            data.Add(new Partner() { Guid = Guid.NewGuid(), Name = "partner 5", CreatedDate = DateTime.Now, Site = SiteToSeed.FirstOrDefault(), CollaboratorRoleId = 2 });
 
-            data.Add(new Partner() { Guid = Guid.NewGuid(), Name = "partner 6", CreatedDate = new DateTime(), Site = SiteToSeed.FirstOrDefault(), CollaboratorRoleId = 3 });
-            data.Add(new Partner() { Guid = Guid.NewGuid(), Name = "partner 7", CreatedDate = new DateTime(), Site = SiteToSeed.FirstOrDefault(), CollaboratorRoleId = 3 });
-            data.Add(new Partner() { Guid = Guid.NewGuid(), Name = "partner 8", CreatedDate = new DateTime(), Site = SiteToSeed.FirstOrDefault(), CollaboratorRoleId = 4 });
+            data.Add(new Partner() { Guid = Guid.NewGuid(), Name = "partner 6", CreatedDate = DateTime.Now, Site = SiteToSeed.FirstOrDefault(), CollaboratorRoleId = 3 });
+            data.Add(new Partner() { Guid = Guid.NewGuid(), Name = "partner 7", CreatedDate = DateTime.Now, Site = SiteToSeed.FirstOrDefault(), CollaboratorRoleId = 3 });
+            data.Add(new Partner() { Guid = Guid.NewGuid(), Name = "partner 8", CreatedDate = DateTime.Now, Site = SiteToSeed.FirstOrDefault(), CollaboratorRoleId = 3 });
 
 
-            data.ForEach(x =>
-            {
 
-                if (new List<string>() { "partner 3", "partner 4", "partner 5" }.Contains(x.Name))
-                {
-                    x.Superior = data.Where(p => p.Name == "partner 1").FirstOrDefault();
-                }
-
-                if (new List<string>() { "partner 6", "partner 7", "partner 8" }.Contains(x.Name))
-                {
-                    x.Superior = data.Where(p => p.Name == "partner 5").FirstOrDefault();
-                }
-            });
 
             return data;
 
         }
-
-
 
         private static List<Household> HouseholdToSeed()
         {
@@ -62,9 +48,7 @@ namespace csi.asp.net.core.data.Seed
 
                 for (int i = 0; i < 30; i++)
                 {
-                    int rondomPartner = new Random().Next(6, 8);
-
-                    data.Add(new Household() {Address =$"Cidade {i}" ,   Guid = Guid.NewGuid(), Name = $@"Household {i}", CreatedDate = new DateTime(), Partner = PartnerToSeed().Where(p => p.Name == $"partner {rondomPartner}").FirstOrDefault() });
+                    data.Add(new Household() { Address = $"Cidade {i}", Guid = Guid.NewGuid(), Name = $@"Household {i}", CreatedDate = DateTime.Now, });
 
                 }
 
@@ -89,9 +73,9 @@ namespace csi.asp.net.core.data.Seed
             {
                 var cb = new List<CollaboratorRole>()
                 {
-                    new CollaboratorRole(){ Description = "Supervisor"},
-                    new CollaboratorRole(){ Description = "chefe"},
-                    new CollaboratorRole(){ Description = "normal"}
+                    new CollaboratorRole(){ Description = "Supervisor" ,Guid = Guid.NewGuid(),CreatedDate =  DateTime.Now, },
+                    new CollaboratorRole(){ Description = "chefe",Guid = Guid.NewGuid(),CreatedDate =  DateTime.Now, },
+                    new CollaboratorRole(){ Description = "normal", Guid = Guid.NewGuid(),CreatedDate =  DateTime.Now, }
                 };
 
                 await db.collaboratorRoles.AddRangeAsync(cb);
@@ -99,9 +83,47 @@ namespace csi.asp.net.core.data.Seed
             }
 
 
+            if (db.partners.Count() == 0)
+            {
+
+                var data = PartnerToSeed();
+                await db.partners.AddRangeAsync(data);
+                await db.SaveChangesAsync();
+
+
+                data.ForEach(x =>
+                {
+
+                    if (new List<string>() { "partner 3", "partner 4", "partner 5" }.Contains(x.Name))
+                    {
+                        x.Superior = data.Where(p => p.Name == "partner 1").FirstOrDefault();
+                    }
+
+                    if (new List<string>() { "partner 6", "partner 7", "partner 8" }.Contains(x.Name))
+                    {
+                        x.Superior = data.Where(p => p.Name == "partner 5").FirstOrDefault();
+                    }
+                });
+
+
+                db.partners.UpdateRange(data);
+
+            }
+
+
             if (db.houseHolds.Count() == 0)
-            { 
-                db.houseHolds.AddRange(HouseholdToSeed());
+            {
+                var data = HouseholdToSeed();
+
+                data.ForEach(x =>
+                {
+                    int rondomPartner = new Random().Next(6, 8); // normal parner id range  
+                    x.PartnerId = db.partners.FirstOrDefault(p => p.Id == rondomPartner).Id;
+
+                });
+
+
+                db.houseHolds.AddRange(data);
                 await db.SaveChangesAsync();
             }
         }

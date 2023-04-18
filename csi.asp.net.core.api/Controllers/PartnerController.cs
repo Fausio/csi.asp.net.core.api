7d4a6efc092fc3ad313708b13b0a48fb8ef2a8bd
Fausio Matsinhe <fausio.matsinhe@kooli.io>
2023-04-18 11:03:17 +0200

    other controlers

--- csi.asp.net.core.api/Controllers/PartnerController.cs
+++ csi.asp.net.core.api/Controllers/PartnerController.cs
@@ -1,15 +1,96 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using csi.asp.net.core.model.model;
+using csi.asp.net.core.service.Interface;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Identity.Web.Resource;
 
 namespace csi.asp.net.core.api.Controllers
 {
 
-    [Authorize]
+    //[Authorize]
     [ApiController]
     [Route("[controller]")]
     [RequiredScope(RequiredScopesConfigurationKey = "AzureAd:Scopes")]
     public class PartnerController
     {
+        private readonly IPartnerInterface _partnerInterface  ;
+
+        public PartnerController(IPartnerInterface partnerInterface)
+        {
+            _partnerInterface = partnerInterface;
+        }
+
+
+        [HttpPost]
+        public async Task<Partner> Create(Partner model)
+        {
+            try
+            {
+                return await _partnerInterface.Create(model);
+
+            }
+            catch (Exception e)
+            {
+
+                throw;
+            }
+        }
+
+        [HttpPut]
+        public async Task<Partner> Update(Partner model)
+        {
+            try
+            {
+                return await _partnerInterface.Update(model);
+
+            }
+            catch (Exception e)
+            {
+
+                throw;
+            }
+        }
+        [HttpGet]
+        public async Task<List<Partner>> Read()
+        {
+            try
+            {
+                return await _partnerInterface.Read();
+
+            }
+            catch (Exception e)
+            {
+
+                throw;
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<Partner> Read(int id)
+        {
+            try
+            {
+                return await _partnerInterface.Read(id);
+
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task ReadById(int id)
+        {
+            try
+            {
+                await _partnerInterface.Delete(id);
+
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
     }
 }
--- csi.asp.net.core.api/Controllers/SiteController.cs
+++ csi.asp.net.core.api/Controllers/SiteController.cs
@@ -1,14 +1,94 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using csi.asp.net.core.model.model;
+using csi.asp.net.core.service.Interface;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Identity.Web.Resource;
 
 namespace csi.asp.net.core.api.Controllers
 {
-    [Authorize]
+    //[Authorize]
     [ApiController]
     [Route("[controller]")]
     [RequiredScope(RequiredScopesConfigurationKey = "AzureAd:Scopes")] 
     public class SiteController
     {
+        private readonly ISiteInterface  _siteInterface;
+
+        public SiteController(ISiteInterface  siteInterface)
+        {
+            _siteInterface = siteInterface;
+        }
+
+        [HttpPost]
+        public async Task<Site> Create(Site model)
+        {
+            try
+            {
+                return await _siteInterface.Create(model);
+
+            }
+            catch (Exception e)
+            {
+
+                throw;
+            }
+        }
+
+        [HttpPut]
+        public async Task<Site> Update(Site model)
+        {
+            try
+            {
+                return await _siteInterface.Update(model);
+
+            }
+            catch (Exception e)
+            {
+
+                throw;
+            }
+        }
+        [HttpGet]
+        public async Task<List<Site>> Read()
+        {
+            try
+            {
+                return  await _siteInterface.Read();
+
+            }
+            catch (Exception e)
+            {
+
+                throw;
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<Site> Read(int id)
+        {
+            try
+            {
+                return await _siteInterface.Read(id);
+
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task ReadById(int id)
+        {
+            try
+            {
+                await _siteInterface.Delete(id);
+
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
     }
 }
